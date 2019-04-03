b9113e3c7c4bb79c3aee0b105059b8d679854403
MattiMatt8 <momatthiasson@gmail.com>
2019-04-03 00:15:00 +0000

    +rep

--- skilaverkefni6_2.py
+++ skilaverkefni6_2.py
@@ -1,25 +1,31 @@
 # Matthías Ólafur
 import csv
 
-with open("triangle.txt","r") as file:
+with open("triangle67.txt","r") as file:
     csv = csv.reader(file, delimiter=' ')
     data = []
     for x in csv:
         data.append(list(map(int, x)))
-print("len",len(data))
 
-def triangle(data,row=0,column=0):
+def triangle_v2(data,row=0,column=0,memo={}):
     if row == len(data):
         return 0
     d = data[row]
-    if row == 0:
-        print("0",d[column])
-        return triangle(data,row+1) + d[column]
-    if d[column] >= d[column+1]:
-        print("2",row,d[column])
-        return triangle(data,row+1,column) + d[column]
+    summa = 0
+    if (str(row+1) + "," + str(column)) in memo:
+        summa1 = memo[str(row+1) + "," + str(column)]
     else:
-        print("4",row,d[column+1])
-        return triangle(data,row+1,column+1) + d[column+1]
+        summa1 = triangle_v2(data,row+1,column,memo)
+    if (str(row + 1) + "," + str(column+1)) in memo:
+        summa2 = memo[str(row + 1) + "," + str(column+1)]
+    else:
+        summa2 = triangle_v2(data,row+1,column+1,memo)
+    if summa1 >= summa2:
+        summa += summa1
+    else:
+        summa += summa2
+    summa += d[column]
+    memo[str(row) + "," + str(column)] = summa
+    return summa
 
-print(triangle(data))
+print(triangle_v2(data))
